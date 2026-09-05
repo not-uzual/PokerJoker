9970dd01e27bade6aa1586c0e5202ba47b08c9bf
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-09-06 01:08:48 +0530

    fix: cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,9 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}));
 
 
 export const httpServer = createServer(app);
