2a4c146d5a7ce07bb3311a4a26d08167e1214d36
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-09-06 01:50:32 +0530

    chore

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000/'
+  origin: 'http://localhost:3000'
 }));
 
 
@@ -16,7 +16,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
   },
 });
 
